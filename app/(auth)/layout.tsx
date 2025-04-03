c68da6726b071e034b8feed19b0ae841153fb029
K.Saurabh Rao <saurabh91798@gmail.com>
2025-04-03 14:18:45 +0530

    implemented the interview page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,11 @@
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 import { redirect } from "next/navigation";
+
 import { isAuthenticated } from "@/lib/actions/auth.action";
 
 const AuthLayout = async ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated();
-  if (!isAuthenticated) redirect("/");
+  if (isUserAuthenticated) redirect("/");
 
   return <div className="auth-layout">{children}</div>;
 };
